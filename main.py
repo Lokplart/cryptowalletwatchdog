8c9e134ca9fb8901499bb681d0586bc4afc88b49
Lokplart <lokplart@gmail.com>
2022-03-15 17:32:04 +0200

    adding BTC - v1.0, working

--- main.py
+++ main.py
@@ -2,33 +2,96 @@ from firebase_admin import credentials, firestore, initialize_app
 from google.cloud.firestore_v1 import Increment
 from flask import Flask, request, render_template
 from threading import Thread, Lock
-from string import digits, ascii_lowercase, ascii_uppercase
-from random import choice
+from uuid import uuid1
+import urllib.request
 import datetime
 import time
+import json
+
+
+def API_BTC(watch_request):
+    url = "https://blockchain.info/rawaddr/" + watch_request.address + "?offset=" + str(watch_request.last_block_checked)
+    trx_data = json.loads(urllib.request.urlopen(url).read().decode())
+
+    if watch_request.last_block_checked < trx_data["n_tx"]:
+        watch_request.last_block_checked = trx_data["n_tx"]
+        requests.document(watch_request.address).update({"last_block_checked": str(trx_data["n_tx"])})
+
+    earliest_starting_time = datetime.datetime.now(tz=datetime.timezone.utc)
+    for watch_data in watch_request.watch_data:
+        if watch_data.processed is False and watch_data.start_date < earliest_starting_time:
+            earliest_starting_time = watch_data.start_date
+
+    for trx in trx_data["txs"]:
+        trx_time = datetime.datetime.fromtimestamp(trx["time"], tz=datetime.timezone.utc)
+        if trx_time < earliest_starting_time and watch_request.address not in trx["inputs"]:
+            break
+
+        print("checking", trx["hash"], "for", watch_request.address)
+
+        for watch_data in watch_request.watch_data:
+            if watch_data.processed is True or trx_time < watch_data.start_date:
+                continue
+
+            if watch_data.sender is not None:
+                match_sender = watch_data.sender in trx["inputs"]
+            else:
+                match_sender = True
+
+            if watch_data.value is not None:
+                match_value = None
+                for out in trx["out"]:
+                    if out["addr"] == watch_request.address:
+                        match_value = out["value"] == watch_data.value
+                        break
+            else:
+                match_value = True
+
+            if match_value and match_sender:
+                watch_data.processed = True
+                watch_data.hash = trx["hash"]
+                requests.document(watch_request.address).collection('data').document(watch_data.data_id).update({
+                    "processed": True,
+                    "hash": watch_data.hash
+                })
+                watch_request.key.update({"active_requests": Increment(-1)})
+                print("--- found", watch_data.hash, "for", watch_data.data_id)
+                break
 
 
 class WatchData:
-    def __init__(self, data_id, sender, value, start_date, trx_hash, processed):
+    def __init__(self, data_id, sender, value, start_date, request_date, trx_hash, processed):
         self.data_id = data_id
         self.sender = sender
-        self.value = value
+        if value is not None:
+            self.value = int(value)
+        else:
+            self.value = None
+        self.request_date = request_date
         self.start_date = start_date
         self.hash = trx_hash
         self.processed = processed
 
     @staticmethod
     def from_dict(data_id, data):
-        return WatchData(data_id, data["sender"], data["value"], data["start_date"], data["hash"], data["processed"])
+        return WatchData(data_id, data["sender"], data["value"], data["start_date"], data["request_date"], data["hash"],
+                         data["processed"])
 
 
 class WatchRequest:
     def __init__(self, address, coin, last_block_checked, key):
         self.address = address
         self.coin = coin
-        self.last_block_checked = last_block_checked
+        self.last_block_checked = int(last_block_checked)
         self.key = key
         self.watch_data = []
+
+    def has_active_requests(self):
+        for watch_data in self.watch_data:
+            if watch_data.processed is False:
+                return True
+
+        return False
 
     @staticmethod
     def from_dict(address, request_data):
@@ -41,25 +104,24 @@ class Watcher(Thread):
 
     def run(self):
         while True:
+            for address in watches:
+                if not watches[address].has_active_requests():
+                    continue
+
+                if watches[address].coin == "btc":
+                    API_BTC(watches[address])
 
             time.sleep(300)
-
-
-def generate_data_id(collection_ref):
-    data_id = ''.join(choice(digits + ascii_lowercase + ascii_uppercase) for _ in range(20))
-    while collection_ref.document(data_id).get().exists:
-        data_id = ''.join(choice(digits + ascii_lowercase + ascii_uppercase) for _ in range(20))
-
-    return data_id
 
 
 def pull():
     global watches
     watches = {}
     for address in requests.stream():
-        watches[address] = (WatchRequest.from_dict(address.id, address.to_dict()))
-        for data in requests.document(address).collection("data").stream():
-            watches[address].watch_data.append(WatchData.from_dict(data.id, data.to_dict()))
+        watches[address.id] = (WatchRequest.from_dict(address.id, address.to_dict()))
+        for data in requests.document(address.id).collection("data").stream():
+            watches[address.id].watch_data.append(WatchData.from_dict(data.id, data.to_dict()))
+        watches[address.id].watch_data.sort(key=lambda x: x.request_date)
 
 
 app = Flask(__name__)
@@ -77,7 +139,7 @@ accepted_coins = ["btc", "eth"]
 request_lock = Lock()
 
 
-@app.route('/api/request', methods=["GET"])
+@app.route('/api/request', methods=["GET", "POST"])
 def api_request():
     request_lock.acquire()
     global watches
@@ -95,7 +157,13 @@ def api_request():
 
     # getting and verifying coin - mandatory
     coin = request.args.get("coin")
-    if coin not in accepted_coins:
+    if coin is not None:
+        coin = coin.lower()
+        if coin not in accepted_coins:
+            request_lock.release()
+            return render_template("main.html", response="invalid coin")
+    else:
+        request_lock.release()
         return render_template("main.html", response="invalid coin")
 
     # getting and verifying value - optional
@@ -106,6 +174,8 @@ def api_request():
             if value <= 0:
                 request_lock.release()
                 return render_template("main.html", response="invalid value")
+            if coin == "btc":
+                value *= 10e+7
         except ValueError:
             request_lock.release()
             return render_template("main.html", response="invalid value")
@@ -117,7 +187,7 @@ def api_request():
     else:
         start_date = start_date.split('-')
         try:
-            start_date = datetime.datetime(year=int(start_date[3]), month=int(start_date[2]), day=int(start_date[0]),
+            start_date = datetime.datetime(year=int(start_date[2]), month=int(start_date[1]), day=int(start_date[0]),
                                            tzinfo=datetime.timezone.utc)
         except ValueError:
             request_lock.release()
@@ -127,6 +197,8 @@ def api_request():
     address = request.args.get("address")
     # getting expected sender address - optional
     sender = request.args.get("sender")
+
+    request_date = datetime.datetime.now(tz=datetime.timezone.utc)
 
     # if new address, add it to db
     if not requests.document(address).get().exists:
@@ -138,26 +210,31 @@ def api_request():
         watches[address] = WatchRequest(address, coin, 0, key)
 
     # storing request
-    data_id = generate_data_id(requests.document(address).collection('data'))
+    data_id = str(uuid1())
     requests.document(address).collection('data').document(data_id).set({
+        'request_date': request_date,
         'start_date': start_date,
         'processed': False,
         'sender': sender,
-        'value': value,
-        'hash': "",
+        'value': str(value),
+        'hash': ""
     })
 
-    watches[address].watch_data.append(WatchData(data_id, sender, value, start_date, "", False))
+    watches[address].watch_data.append(WatchData(data_id, sender, value, start_date, request_date, "", False))
+    watches[address].watch_data.sort(key=lambda x: x.request_date)
 
     key.update({'active_requests': Increment(1)})
 
     request_lock.release()
-    return render_template("main.html", response="request approved")
+    return render_template("main.html", response=data_id)
 
 
 @app.route('/')
 def index():
     return 'Web App with Python Flask!'
 
+
+watcher = Watcher()
+watcher.start()
 
 app.run(threaded=True, host='0.0.0.0', port=8000)
